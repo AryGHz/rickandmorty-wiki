57b70e73b930c548994edbfb0812dd00e813e87b
AryGHz <ary_gh@hotmail.com>
2024-05-19 14:59:49 -0600

    adding tablename to models

--- Server/src/models/Favorite.js
+++ Server/src/models/Favorite.js
@@ -3,54 +3,43 @@ const { DataTypes } = require('sequelize');
 
 const Favorite = (sequelize) => {
    sequelize.define('Favorite', {
-    id:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-         
-    },
-    api_id:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    name:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    // status:{
-    //     type: DataTypes.ENUM('Alive', 'Dead', 'unknown'),
-    //     allowNull: false,
+            id:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+                
+            },
+            api_id:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            name:{
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            image:{
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            type:{
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: 'type cannot be an empty string'
+                    }
+                }
         
-    // },
-    // species:{
-    //     type: DataTypes.STRING,
-    //     allowNull: false,
-    // },
-    // gender:{
-    //     type: DataTypes.ENUM('Female', 'Male', 'Genderless','Unknown'),
-    //     allowNull: false,
-        
-    // },
-    // origin:{
-    //     type: DataTypes.STRING,
-        
-    // },
-    image:{
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    type:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: {
-                msg: 'type cannot be an empty string'
-            }
-        }
-       
-    },
-   }, { timestamps: true });
+            },
+        }, 
+        { 
+            timestamps: true 
+        },
+        { 
+            tablename: "favorites" 
+        },
+    );
 };
 
 
